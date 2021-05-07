53e1abd74fa5702e6f8f29735e0ba163ba8a2e08
carlosmolinadev <carlosmolina.dev@gmail.com>
2021-05-07 10:49:18 -0600

    finished project

--- app/src/main/java/com/carlos/ahorromatic/ui/usuario/UsuarioFragment.kt
+++ app/src/main/java/com/carlos/ahorromatic/ui/usuario/UsuarioFragment.kt
@@ -9,6 +9,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
@@ -18,6 +19,7 @@ import com.carlos.ahorromatic.R
 import com.carlos.ahorromatic.RegistroApplication
 import com.carlos.ahorromatic.db.entities.UsuarioEntity
 import com.carlos.ahorromatic.RegistroApplication.Companion
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 
 class UsuarioFragment : Fragment(), UsuarioAdapter.AdapterClickListeners {
@@ -41,7 +43,11 @@ class UsuarioFragment : Fragment(), UsuarioAdapter.AdapterClickListeners {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         val usuarios = viewModel.usuarios
-
+        val fab = view.findViewById<FloatingActionButton>(R.id.fab)
+        fab.setOnClickListener {
+            viewModel.usuarioActual = null
+            findNavController().navigate(R.id.action_nav_usuario_to_nav_guardar_usuario)
+        }
 
         //Recycler View
         val adapter = UsuarioAdapter(this)
